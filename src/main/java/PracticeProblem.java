33c8dd0b5d66c2f732cbd269e151d4b9b1ef9958
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-19 18:09:40 +0000

    2.1 complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,38 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+	public static int find(int[] array, int number){
+		
+		for (int i = 0; i<array.length; i++){
+			if (array[i] == number){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findLast(String[] array, String string){
+		int index =-1;
+		for (int i =0; i < array.length; i++){
+			if (array[i].equals(string)){
+				index =i;
+			}
+		}
+		return index;
+	}
+	public static int findSecond(char[] array, char character){
+		int occurence =0;
+		int index =-1;
+		for (int i =0; i<array.length; i++){
+			if (array[i]== character){
+				index=i;
+				occurence++;
+				if (occurence ==2){
+					return index;
+				}
+			}
+		}
+		return index;
+	}
 	
 
 }
